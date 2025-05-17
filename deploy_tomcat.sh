4192a8996fc797d9126ce733739fd0c6dbc4a651
udodi05 <spy.henrygrey+git@gmail.com>
2025-05-17 12:52:48 -0400

    Create deploy_tomcat.sh

---
+++ deploy_tomcat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+echo "Deploying webapp containers..."
+docker pull kniru/tomcat:latest
+
+echo "Creating network"
+docker network create acada-app
+
+for i in {1..6};
+do
+docker stop acada-webapp$i ; docker rm -f acada-webapp$i || true
+docker run -d -p 808$i:8080 --name acada-webapp$i --hostname acada-webapp$i --network acada-app kniru/tomcat:latest;
+echo "Deploying webapp$i container done"
+done
+
+sleep 10
+
+echo "Deploying HAproxy container..."
+docker rm haproxy -f >/dev/null 2>&1 || true
+docker run -d --name haproxy --network acada-app -v /opt/docker_config_files/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro -p 9090:80 haproxy:latest
+docker ps | grep -i haproxy*
+echo "Deploying HAproxy container done"
